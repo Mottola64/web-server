349f58795d9f1e2180f7c8b1a5640dc922f2e78f
Michael Mottola <mottola64@gmail.com>
2020-11-12 18:35:38 -0500

    json endpoints

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const { dirname } = require('path')
+const { response } = require('express')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -32,7 +35,32 @@ app.get('/help', (req, res) => {
 
 
 app.get('/weather', (req, res) => {
-    res.send({forecast: "Cloudy", location: "RVC"})
+    
+    if (!req.query.address) {
+        return res.send({error: "You must provide an address!"})
+    } else {
+        geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+            if (error) {
+              return res.send({error})
+            }
+            forecast(latitude, longitude, (error, forecastData) => {
+              if (error) {
+                return res.send({error})
+              }
+          
+              res.send({location, forecast: forecastData, address: req.query.address})
+            })
+          })
+    }
+})
+
+app.get('/products', (req, res) => {
+
+    if (!req.query.search) {
+        return res.send({error: 'You must provide a search term'})
+    }
+    console.log(req.query.search)
+    res.send({products: []})
 })
 
 app.get('/help/*', (req, res) => {
