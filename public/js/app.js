f6d1535e004e4b95cf0090f1c01971011514f68f
Michael Mottola <mottola64@gmail.com>
2020-11-13 15:45:52 -0500

    fetch requests and search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,29 @@
 console.log('client side js file loaded')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+const fetchByLocation = (location) => {
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})}
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    console.log(location)
+    fetchByLocation(location)
+})
