daea41dc7b07d9b45d4c1a2fd2902062ad63489c
Michael Mottola <mottola64@gmail.com>
2020-11-13 16:21:02 -0500

    rendering data in html

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const { dirname } = require('path')
 const { response } = require('express')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
@@ -38,20 +37,19 @@ app.get('/weather', (req, res) => {
     
     if (!req.query.address) {
         return res.send({error: "You must provide an address!"})
-    } else {
-        geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+    } 
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-              return res.send({error})
+            return res.send({error})
             }
-            forecast(latitude, longitude, (error, forecastData) => {
-              if (error) {
-                return res.send({error})
-              }
-          
-              res.send({location, forecast: forecastData, address: req.query.address})
-            })
-          })
-    }
+        
+            res.send({location, forecast: forecastData, address: req.query.address})
+        })
+    })
 })
 
 app.get('/products', (req, res) => {
